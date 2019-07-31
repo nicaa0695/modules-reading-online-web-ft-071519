823b1f9bdaaed7ce23dfb7151c19a064c4575006
Veronica Lopez <nicaa0695@gmail.com>
2019-07-31 19:18:37 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,10 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 class Kid 
-  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
   
   attr_accessor :name
  
